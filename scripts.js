467a8831baf2760ace55468b64c9c7205c2c8916
Leonardo Gama <leogama09@gmail.com>
2023-01-11 11:43:51 -0300

    herança

--- scripts.js
+++ scripts.js
@@ -197,3 +197,31 @@ console.log(shirt.productWithDiscount(50))
 const tenis = new Product("Tenis verde", 120)
 
 console.log(tenis.productWithDiscount(20))
+
+
+// 9- Herança
+class ProductWithAttributes extends Product {
+    constructor(name, price, colors) {
+        super(name, price)
+        this.colors = colors
+    }
+
+    showColors() {
+        console.log("As cores são: ")
+        this.colors.forEach((color) => {
+            console.log(color)
+        })
+    }
+}
+
+const hat = new ProductWithAttributes("Chapéu", 29.99, [
+    "Preto", 
+    "Azul", 
+    "Verde",
+])
+
+console.log(hat.name)
+
+console.log(hat.productWithDiscount(30))
+
+hat.showColors()
