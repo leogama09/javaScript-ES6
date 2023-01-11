62b1586b9f7bb84a8ca774e52dcb73428f36fb5c
Leonardo Gama <leogama09@gmail.com>
2023-01-11 11:35:29 -0300

    classes

--- scripts.js
+++ scripts.js
@@ -172,3 +172,28 @@ const otherInfos = {km: 100000, price: 49000}
 const car = {...carName, ...carBrand, ...otherInfos, wheels: 4}
 
 console.log(car)
+
+
+// 8- Classes
+class Product {
+    constructor(name, price) {
+        this.name = name
+        this.price = price
+    }
+
+    productWithDiscount(discount) {
+        return this.price * ((100 - discount) / 100)
+    }
+}
+
+const shirt = new Product("Camisa gola V", 20)
+
+console.log(shirt.name)
+
+console.log(shirt.productWithDiscount(10))
+
+console.log(shirt.productWithDiscount(50))
+
+const tenis = new Product("Tenis verde", 120)
+
+console.log(tenis.productWithDiscount(20))
