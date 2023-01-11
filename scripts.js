8648d141df041da1846c03b028da97b4ab170b59
Leonardo Gama <leogama09@gmail.com>
2023-01-11 11:01:16 -0300

    map

--- scripts.js
+++ scripts.js
@@ -109,3 +109,20 @@ const notAvailableUsers = users.filter((user) => !user.available)
 
 console.log(availableUsers)
 console.log(notAvailableUsers)
+
+
+// 4- Map
+const products = [
+    {name: 'Camisa', price: 10.99, category: 'Roupas'},
+    {name: 'Chaleira  Elétrica', price: 49.99, category: 'Eletro'},
+    {name: 'Fogão', price: 400, category: 'Eletro'},
+    {name: 'Calça jeans', price: 50.99, category: 'Roupas'},
+]
+
+products.map((product) => {
+    if(product.category === 'Roupas') {
+        product.onSale = true
+    }
+})
+
+console.log(products)
